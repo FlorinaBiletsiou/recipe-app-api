9d5eeca37b8abcc752896fe4a05441e8345160a0
Florina Biletsiou <Florina.Biletsiou@finsa-uk.local>
2019-04-10 16:10:24 +0100

    Added endpoint fro creating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,7 +30,7 @@ class AdminSiteTests(TestCase):
     def test_user_change_page(self):
         # test that the user edit page works
         url = reverse('admin:core_user_change', args=[self.user.id])
-        #/admin/core/user/user.id/
+        # /admin/core/user/user.id/
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
