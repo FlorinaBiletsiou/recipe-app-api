a1245c95324a6d15425bf435ee71b152294886ec
Florina Biletsiou <Florina.Biletsiou@finsa-uk.local>
2019-04-12 17:25:57 +0100

    Added recipe image upload feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -81,7 +81,8 @@ class ModelTests(TestCase):
         # Test that image is saved in the correct location
         uuid = 'test-uuid'
         mock_uuid.return_value = uuid
-        file_path = models.recipe_image_file_path(None, 'my_image.jpg')
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
 
-        exp_path = f'upload/recipe/{uuid}.jpg'
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+
         self.assertEqual(file_path, exp_path)
